27e923286ae8c872b3f13ab7f26f2b535c0d1090
masumahmadb <masumahmadb@gmail.com>
2026-08-21 16:47:14 +0530

    Fix navbar routing on Vercel with SPA rewrite + remove stale duplicate folder

--- assam-jobs-repository/src/services/pushNotifications.js
+++
@@ -1,6 +0,0 @@
-import { registerForPush, listenForForegroundMessages } from '../firebase/messaging.js'
-
-export async function initPushNotifications(uid, onForegroundMessage) {
-  await registerForPush(uid)
-  await listenForForegroundMessages(onForegroundMessage)
-}
--- assam-jobs-repository/src/utils/districts.js
+++
@@ -1,7 +0,0 @@
-export const ASSAM_DISTRICTS = [
-  'Baksa','Barpeta','Biswanath','Bongaigaon','Cachar','Charaideo','Chirang','Darrang',
-  'Dhemaji','Dhubri','Dibrugarh','Dima Hasao','Goalpara','Golaghat','Hailakandi',
-  'Hojai','Jorhat','Kamrup Metropolitan','Kamrup','Karbi Anglong','Karimganj',
-  'Kokrajhar','Lakhimpur','Majuli','Morigaon','Nagaon','Nalbari','Sivasagar',
-  'Sonitpur','South Salmara-Mankachar','Tinsukia','Udalguri','West Karbi Anglong'
-]
